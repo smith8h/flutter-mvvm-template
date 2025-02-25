085e66c7b47af633d604b4806f0191d87b7ba12c
Hussein Shakir <husseinshakir81@gmail.com>
2025-02-26 02:37:08 +0300

    feat: Add DeviceUtils class for device-related utility functions

---
+++ lib/core/utils/device_utils.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+class DeviceUtils {
+  static void hideKeyboard() {
+    FocusScope.of(Get.context!).requestFocus(FocusNode());
+  }
+
+  static bool isPortraitOrientation() {
+    final viewInsets = View.of(Get.context!).viewInsets;
+    return viewInsets.bottom != 0;
+  }
+
+  static void setFullScreen(bool enable) {
+    SystemChrome.setEnabledSystemUIMode(enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge);
+  }
+
+  static double getScreenHeight() {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static double getScreenWidth() {
+    return MediaQuery.of(Get.context!).size.width;
+  }
+
+  static double getPixelRatio() {
+    return MediaQuery.of(Get.context!).devicePixelRatio;
+  }
+
+  static double getStatusBarHeight() {
+    return MediaQuery.of(Get.context!).padding.top;
+  }
+
+  static double getBottomNavigationBarHeight() {
+    return kBottomNavigationBarHeight;
+  }
+
+  static double getAppBarHeight() {
+    return kToolbarHeight;
+  }
+
+  static double getKeyboardHeight() {
+    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+    return viewInsets.bottom;
+  }
+
+  static Future<bool> isKeyboardVisible() async {
+    final viewInsets = View.of(Get.context!).viewInsets;
+    return viewInsets.bottom > 0;
+  }
+
+  static Future<bool> isPhysicalDevice() async {
+    return defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS;
+  }
+
+  static void vibrate(int msec) {
+    HapticFeedback.vibrate();
+    Future.delayed(Duration(microseconds: msec), () => HapticFeedback.vibrate());
+  }
+
+  static Future<void> setPreferredOrientations(List<DeviceOrientation> orientations) async {
+    await SystemChrome.setPreferredOrientations(orientations);
+  }
+
+  static void hideStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  static void showStatusBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+  }
+
+  static Future<bool> hasInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+}
